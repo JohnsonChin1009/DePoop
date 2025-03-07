77d9de991e43a1bb0c0cb925321634bb52399541
siaji526 <siaji526@gmail.com>
2025-03-07 16:40:54 +0800

    feat: depoop deployed on graph

--- backend/depoop/src/poop-tracker.ts
+++ backend/depoop/src/poop-tracker.ts
@@ -1,19 +1,19 @@
-import { PoopEventLogged as PoopEventLoggedEvent } from "../generated/PoopTracker/PoopTracker"
-import { PoopEventLogged } from "../generated/schema"
+import { PoopEventLogged as PoopEventLoggedEvent } from '../generated/PoopTracker/PoopTracker';
+import { PoopEventLogged } from '../generated/schema';
 
 export function handlePoopEventLogged(event: PoopEventLoggedEvent): void {
-  let entity = new PoopEventLogged(
+  const entity = new PoopEventLogged(
     event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.user = event.params.user
-  entity.latitude = event.params.latitude
-  entity.longitude = event.params.longitude
-  entity.timestamp = event.params.timestamp
-  entity.sessionDuration = event.params.sessionDuration
+  );
+  entity.user = event.params.user;
+  entity.latitude = event.params.latitude;
+  entity.longitude = event.params.longitude;
+  entity.timestamp = event.params.timestamp;
+  entity.sessionDuration = event.params.sessionDuration;
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
 
-  entity.save()
+  entity.save();
 }
